4136c58b5ea869da2ccf637dd4a2349720ee8d08
Amanda Castro <akamillycs@gmail.com>
2025-05-04 00:31:44 -0300

    Update Exercício 5 - Distribuições: Normal, Binomial e Poisson.py

--- iniciante/Exercícios/Exercício 5 - Distribuições: Normal, Binomial e Poisson.py
+++ iniciante/Exercícios/Exercício 5 - Distribuições: Normal, Binomial e Poisson.py
@@ -1,3 +1,4 @@
+# Normal
 import matplotlib.pyplot as plt
 
 # Gere 1000 números com distribuição normal
@@ -6,3 +7,18 @@ normal = np.random.normal(loc=0, scale=1, size=1000)
 plt.hist(normal, bins=30, density=True)
 plt.title("Distribuição Normal")
 plt.show()
+
+# Binomial
+binomial = np.random.binomial(n=10, p=0.5, size=1000)
+
+plt.hist(binomial, bins=10)
+plt.title("Distribuição Binomial (n=10, p=0.5)")
+plt.show()
+
+# Poisson
+poisson = np.random.poisson(lam=3, size=1000)
+
+plt.hist(poisson, bins=15)
+plt.title("Distribuição de Poisson (λ=3)")
+plt.show()
+

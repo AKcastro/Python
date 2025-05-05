c5d9405207323cb21c4d7d4872d27b624003403e
Amanda Castro <akamillycs@gmail.com>
2025-05-05 13:16:39 -0300

    Create Exercício 6 - Condicionais.py

---
+++ iniciante/Exercícios/Exercício 6 - Condicionais.py
@@ -0,0 +1,14 @@
+### Desafio:Peça para o usuário digitar um número.
+# Verifique: Se o número for positivo, imprima "Número positivo!"
+# Se for negativo, imprima "Número negativo!"
+# Se for zero, imprima "Zero!"
+
+Código:
+n = float(input("Digite um número:"))
+
+if n >0:
+    print("Núemro positivo!")
+elif n<0:
+    print("Número negativo!")
+elif n ==0:
+    print("Zero!")
